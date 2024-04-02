1941cd543d3f94b925cc5a436764b162d1de193d
thechitransh <thechitranshsrivastava@gmail.com>
2024-04-02 19:02:56 +0530

    add form submition

--- src/Components/Regitration.jsx
+++ src/Components/Regitration.jsx
@@ -1,50 +1,88 @@
+import { useState } from "react";
 import React from "react";
 import Navbar from "./Navbar";
 import "./RegistrationStyle.css";
+import axios from "axios";
 
 function Regitration() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    username: "",
+  });
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <>
       <Navbar />
       <main>
-        <div className="form">
+        <div>
           <section className="heading">
             <p>Register Now</p>
           </section>
-          <form action="">
-            <section className="input1">
-              <div>
-                <p>Full name</p>
-                <input type="text" />
-              </div>
-              <div>
-                <p>E-mail</p>
-                <input type="email" />
-              </div>
-              <div>
-                <p>Password</p>
-                <input type="password" />
-              </div>
-            </section>
-            <section className="input2">
-              <div>
-                <p>User Name</p>
-                <input type="text" />
-              </div>
-              <div>
-                <p>Role</p>
-                <select>
-                  <option value="mentor">Mentor</option>
-                  <option value="student">Student</option>
-                </select>
-              </div>
-              <div>
-                <p>Confirm Password</p>
-                <input type="password" />
-              </div>
-            </section>
+          <form onSubmit={handleSubmit}>
+            <div className="form-input">
+              <section className="input1">
+                <div>
+                  <p>Full name</p>
+                  <input
+                    name="name"
+                    type="text"
+                    value={formData.name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <p>E-mail</p>
+                  <input
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <p>Password</p>
+                  <input type="password" />
+                </div>
+              </section>
+              <section className="input2">
+                <div>
+                  <p>User Name</p>
+                  <input
+                    name="username"
+                    type="text"
+                    value={formData.username}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <p>Role</p>
+                  <select>
+                    <option value="mentor">Mentor</option>
+                    <option value="student">Student</option>
+                  </select>
+                </div>
+                <div>
+                  <p>Confirm Password</p>
+                  <input
+                    name="password"
+                    type="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                </div>
+              </section>
+            </div>
+            <button type="submit">Submit</button>
           </form>
-          <button>Submit</button>
         </div>
       </main>
     </>
